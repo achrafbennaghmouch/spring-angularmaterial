6e8faddccacac5cd5758991ccbf1eb7624fe5d1b
Achrafbn8 <128222750+Achrafbn8@users.noreply.github.com>
2024-04-16 12:22:37 +0100

    1st

---
+++ src/main/java/org/example/springangularmaterial/SpringAngularmaterialApplication.java
@@ -0,0 +1,13 @@
+package org.example.springangularmaterial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularmaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularmaterialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springangularmaterial/SpringAngularmaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springangularmaterial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularmaterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
